f65575b9046a7a596ce24e4ea934b3d6b7aac5fb
tuheen66 <hassan.monirul@gmail.com>
2024-09-13 21:18:18 +0600

    making some adjustment to dashboard layout

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -16,19 +16,6 @@ const Dashboard = () => {
     logOut().then(() => console.log("logged out successfully "));
   };
 
-  const { data: users = [] } = useQuery({
-    queryKey: ["users"],
-    queryFn: async () => {
-      const res = await axiosSecure.get("/users");
-      return res.data;
-    },
-  });
-
-  const admin = users.filter((user) => user.role === "admin");
-  const adminUser = admin.map((user) => user.email);
-
- 
-
   return (
     <div className="w-[90%] mx-auto flex flex-col lg:flex-row">
       <Helmet>
@@ -96,13 +83,6 @@ const Dashboard = () => {
       </div>
       <div className="flex-1">
         <Outlet></Outlet>
-        <div className="min-h-screen">
-          <h2 className="font-bold text-5xl text-center text-gray-700 my-52 uppercase ">
-            {adminUser.includes(user.email)
-              ? "Admin dashboard home "
-              : "User dashboard home"}
-          </h2>
-        </div>
       </div>
     </div>
   );
